aa50af985e0249018fc02fe57599cab5508ba353
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-08-09 07:38:38 -0300

    comecando um menu

--- biblioteca/src/Cadastro_de_livros.java
+++ biblioteca/src/Cadastro_de_livros.java
@@ -1,24 +1,19 @@
 public class Cadastro_de_livros {
     public static void main(String[] args) throws Exception {
 
+        Registro reg = new Registro();
         Autor autor1 = new Autor();
         autor1.setNome("Stephen King");
         autor1.setEmail("StKing@yahoo.com");
         autor1.setIdade(74);
 
         Livro livro1 = new LivroFisico(autor1);
-        livro1.setNome("O instituto");
-        livro1.setDescricao("Livro de suspense");
-        livro1.setValor(50);
-        livro1.setCodigo("978-10-82198-11-9");
+        reg.novo(livro1);
 
         livro1.mostrarDados();
 
         Livro livro2 = new LivroFisico(autor1);
-        livro2.setNome ("It a coisa");
-        livro2.setDescricao("Livro de terror");
-        livro2.setValor(44.50);
-        livro2.setCodigo("124-42-15232-13-6");
+        reg.novo(livro2);
 
         livro2.mostrarDados();
         livro2.aplicaDesconto(0.12);
@@ -30,11 +25,7 @@ public class Cadastro_de_livros {
         autor2.setIdade(29);
 
         Ebook ebook1 = new Ebook(autor2);
-        ebook1.setNome("Ordem Paranormal, o comeco");
-        ebook1.setDescricao("Terror e medo");
-        ebook1.setValor(100);
-        ebook1.setCodigo("193-15-51374-64-8");
-        ebook1.setWaterMark("Cellbit");
+        reg.novo(ebook1);
 
         ebook1.aplicaDesconto(0.32);
 
---
+++ biblioteca/src/Registro.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Registro {
+
+    public void novo(Livro livro){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("-----------------------------");
+        System.out.println("Adicionando novo livro");
+
+        System.out.println("Insira o nome do livro: ");
+        livro.setNome(scan.nextLine());
+
+        System.out.println("Insira a descricao do livro: ");
+        livro.setDescricao(scan.nextLine());
+
+        System.out.println("Insira o preco do livro: ");
+        livro.setValor(scan.nextDouble());
+
+        System.out.println("Insira o codigo do livro: ");
+        livro.setCodigo(scan.nextLine());
+        System.out.println("-----------------------------");
+        scan.close();
+    }
+    
+}
+
