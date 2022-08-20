6106cca512f9b39f3b41defb5413acd3de50a2da
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-08-20 17:14:44 -0300

    teste

--- player_musicas/Main.java
+++ player_musicas/Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
         TrackReader trackreader = new TrackReader();
         trackreader.readTracks("C:\\GitHub\\Java-OO\\player_musicas\\audio", ".mp3");
         MusicOrganizer organizador = new MusicOrganizer();
