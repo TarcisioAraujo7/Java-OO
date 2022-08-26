f1a98f881383df8d8b37c7bae56b39f2147951c6
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-08-25 22:46:45 -0300

    primeira classe

---
+++ xadrez/src/aplicacao/principal.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class principal {
+    public static void main(String[] args) {
+        Posicao p1 = new Posicao(1, 1);
+        System.out.println(p1);
+    }
+    
+}
