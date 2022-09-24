13393dd19ca2bbdb63713ceee7e3a8de20175e12
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-09-23 21:08:11 -0300

    Create InteracaoTest.java

---
+++ zull-good/src/testes/InteracaoTest.java
@@ -0,0 +1,24 @@
+package testes;
+import org.junit.Test;
+
+public class InteracaoTest {
+    @Test
+    public void testCheckDeathRoom() {
+
+    }
+
+    @Test
+    public void testInteract() {
+
+    }
+
+    @Test
+    public void testSetDeathRoom() {
+
+    }
+
+    @Test
+    public void testSetDialogRoom() {
+
+    }
+}
