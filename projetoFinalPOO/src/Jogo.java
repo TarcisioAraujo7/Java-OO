4114aa184a4112a3ebb2b98b297653957c405480
Tarcisio Almeida <91756723+TarcisioAraujo7@users.noreply.github.com>
2022-10-27 11:53:34 -0300

    projeto

---
+++ projetoFinalPOO/src/Jogo.java
@@ -0,0 +1,8 @@
+public class Jogo extends Produto {
+
+    public Jogo(String nome, String descricao) {
+        super(nome, descricao);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ projetoFinalPOO/src/prin.java
@@ -0,0 +1,8 @@
+public class prin {
+    public static void main(String[] args) {
+        Produto p1 = new Produto("JOGO", "JOGO MT BOM");
+        System.out.println( p1.getId() );
+        Produto p2 = new Produto("JOO", "JOGO MT BOM");
+        System.out.println( p2.getId() );
+     }
+}
