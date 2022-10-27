256f1e065a35c1433eaab2f0a79d59b52c0f0adc
Tarcisio Almeida <91756723+TarcisioAraujo7@users.noreply.github.com>
2022-10-27 13:46:27 -0300

    jogo

--- projetoFinalPOO/src/Jogo.java
+++ projetoFinalPOO/src/Jogo.java
@@ -1,8 +1,17 @@
+import java.util.List;
+
 public class Jogo extends Produto {
+
+    private int Progresso = 0;
+    private List<Conquista> conquistas = new Arraylist<Conquista>();
 
     public Jogo(String nome, String descricao) {
         super(nome, descricao);
-        //TODO Auto-generated constructor stub
     }
     
+    public void Jogar(int horasJogadas){
+        if (Progresso <= 100) {
+            Progresso += horasJogadas;
+        }
+    }
 }
