1663c0f016c503006f2840324b6b219c12365c40
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-11-01 20:07:24 -0300

    ai calica

---
+++ projeto/src/AlbumMusical.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlbumMusical extends Produto {
+
+    private List<Musica> musicas = new ArrayList<Musica>();
+
+    public AlbumMusical(String nome, String descricao) {
+        super(nome, descricao);
+    }
+    
+}
---
+++ projeto/src/Jogo.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jogo extends Produto {
+
+    private int Progresso = 0;
+    private int HorasJogadas = 0;
+
+    private List<Conquista> conquistas = new ArrayList<Conquista>();
+
+    public Jogo(String nome, String descricao) {
+        super(nome, descricao);
+    }
+    
+    public void Jogar(int horasJogadas){
+        if (Progresso <= 100) {
+            Progresso += horasJogadas;
+        }
+        this.HorasJogadas += horasJogadas;
+    }
+
+    public void setConquista(String nome, String descricao, int progressoAObter){
+        Conquista novaConquista = new Conquista(nome, descricao, progressoAObter);
+        conquistas.add(novaConquista);
+    }
+}
