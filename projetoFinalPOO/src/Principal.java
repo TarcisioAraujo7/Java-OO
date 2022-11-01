0b19c01f5856154b45ce95cf25e21150368f0574
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-11-01 20:04:50 -0300

    att

--- projetoFinalPOO/src/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-
-        Jogador teste = new Jogador("a", "1");
-        
-        System.out.println(teste.confereSenha("12"));
-
-    }
-}
