2901d9ba94ef1c4b7cb1ff87b429f3fb522e0f13
Tarcisio Almeida <tarcisio.olv@gmail.com>
2022-11-01 22:11:00 -0300

    :+1:

--- projeto/src/AlbumMusical.java
+++ projeto/src/AlbumMusical.java
@@ -6,10 +6,13 @@ public class AlbumMusical extends Produto {
     private List<Musica> musicas = new ArrayList<Musica>();
 
     public AlbumMusical(String nome, String descricao) {
-
         super(nome, descricao);
     }
 
+    public void setMusica(String nome){
+        Musica musica = new Musica(nome);
+        musicas.add(musica);
+    }
     public List<Musica> getMusicas() {
         return musicas;
     }
--- projeto/src/Biblioteca.java
+++ projeto/src/Biblioteca.java
@@ -1,2 +1,25 @@
+import java.util.List;
+
 public class Biblioteca {
+
+    private Loja loja = new Loja();
+    Sessao sessaoAtual;
+
+    public Biblioteca(){
+        Jogo doom = new Jogo("Doom", "Jogo de ação e aventura");
+        doom.setConquista("Primeiro chefão derrotado!", "Parabens! Você derrotou o primeiro BOSS.", 15);
+        doom.setConquista("Metade do jogo concluido!", "Parabens! Você chegou na metade do jogo.", 50);
+        doom.setConquista("JOGO FINALIZADO!", "Parabens! Você derrotou o BOSS final e zerou o jogo.", 100);
+
+        loja.addJogo(doom);
+
+        AlbumMusical albumDoom = new AlbumMusical("Album Doom", "Album do jogo Doom");
+        albumDoom.setMusica("The Only Thing They Fear Is You");
+        albumDoom.setMusica("BFG Division");
+        albumDoom.setMusica("Metahook");
+
+        loja.addAlbum(albumDoom);
+
+        sessaoAtual = new Sessao();
+    }
 }
